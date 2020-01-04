8dc2cd2cb2390ea1c9dfd524d0759b033dcee5b3
TLovell <trevorlovelldesign@gmail.com>
2020-01-04 15:38:45 -0700

    main loop, choosing, undoing is written

--- merge.py
+++ merge.py
@@ -1,7 +1,10 @@
 import curses
 
-class Merge:
+numrow = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+def ordchr(num):
+    return ord(str(num))
 
+class Merge:
 
     def __init__(self, seperate_files, merge_file):
 
@@ -12,25 +15,39 @@ class Merge:
         self.mlist = []
         self.ops = []
     
-    def main_loop(self, scr):
+    def main_loop(self, stdscr):
         while True:
-            scr.clear()
-            scr.addstr(1, len(self.slists) + 1, 'Which item goes next?')
+            stdscr.clear()
+            height,width = stdscr.getmaxyx()
+            
+            available = []
             for i in range(len(self.slists)):
-                scr.addstr(1, len(self.slists) - i, '[%d] %s' % (i, self.slists[i][0]))
+                y = height - len(self.slists) - 1 + i
+                if len(self.slists[i]) > 0:
+                    stdscr.addstr(height - len(self.slists) - 1 + i, 2, '[%d] %s' % (numrow[i], self.slists[i][0]))
+                    available.append(numrow[i])
+                else:
+                    stdscr.addstr(y, 2, '~%d~' % numrow[i])
+            stdscr.addstr(height - len(self.slists) - 2, 0, 'Which item goes next?  [u] undo  [q] quit')
         
             for i in range(len(self.mlist)):
-                scr.addstr(1, len(self.slists) + 2 + len(self.mlist) - i, self.mlist[i])
+                stdscr.addstr(height - len(self.slists) - 4 - len(self.mlist) + i, 0, self.mlist[i])
         
-            scr.refresh()
+            stdscr.refresh()
 
-            c = scr.getch()
+            c = stdscr.getch()
             if c == ord('q'):
                 break
+            elif c == ord('u'):
+                self.undo()
+            elif c in list(map(ordchr, available)):
+                self.choose(numrow.index(int(chr(c))))
 
 
-    def choose(self):
-        pass
+    def choose(self, c):
+        self.mlist.append(self.slists[c].pop(0))
+        self.ops.append(c)
     
     def undo(self):
-        pass
+        if len(self.ops) > 0:
+            self.slists[self.ops.pop()].insert(0, self.mlist.pop())
