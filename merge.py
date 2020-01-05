67e50b046e54f183738fb3c81e644422e88ee6db
TLovell <trevorlovelldesign@gmail.com>
2020-01-04 22:30:12 -0700

    Working commit

--- merge.py
+++ merge.py
@@ -1,8 +1,8 @@
 import curses
 
 numrow = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-def ordchr(num):
-    return ord(str(num))
+def rowget(index):
+    return numrow[index]
 
 class Merge:
 
@@ -12,42 +12,77 @@ class Merge:
         for f in seperate_files:
             self.slists.append(f.readlines())
         
+        self.merge_file = merge_file
+
         self.mlist = []
         self.ops = []
+        self.written_ops = 0
     
     def main_loop(self, stdscr):
         while True:
-            stdscr.clear()
-            height,width = stdscr.getmaxyx()
-            
-            available = []
-            for i in range(len(self.slists)):
-                y = height - len(self.slists) - 1 + i
-                if len(self.slists[i]) > 0:
-                    stdscr.addstr(height - len(self.slists) - 1 + i, 2, '[%d] %s' % (numrow[i], self.slists[i][0]))
-                    available.append(numrow[i])
-                else:
-                    stdscr.addstr(y, 2, '~%d~' % numrow[i])
-            stdscr.addstr(height - len(self.slists) - 2, 0, 'Which item goes next?  [u] undo  [q] quit')
-        
-            for i in range(len(self.mlist)):
-                stdscr.addstr(height - len(self.slists) - 4 - len(self.mlist) + i, 0, self.mlist[i])
-        
-            stdscr.refresh()
-
-            c = stdscr.getch()
-            if c == ord('q'):
+            options = self.draw(stdscr)
+            if not self.get_input(stdscr):
                 break
-            elif c == ord('u'):
-                self.undo()
-            elif c in list(map(ordchr, available)):
-                self.choose(numrow.index(int(chr(c))))
 
+    def draw(self, stdscr):
+        stdscr.clear()
+        height,width = stdscr.getmaxyx()
+        
+        stdscr.addstr(height - len(self.slists) - 2, 0, 'Which item goes next?  [u] undo  [q] quit  [w] write')
+        stdscr.addstr(height - 1, width - 25, self.get_status())
+
+        options = self.get_options()
+        for i in range(len(self.slists)):
+            y = height - len(self.slists) - 1 + i
+            if i in options:
+                stdscr.addstr(y, 2, '[%d] %s' % (numrow[i], self.slists[i][0]))
+            else:
+                stdscr.addstr(y, 2, '~%d~' % numrow[i])
+
+        for i in range(len(self.mlist)):
+            stdscr.addstr(height - len(self.slists) - 4 - len(self.mlist) + i, 0, '%d. %s' % (i + 1, self.mlist[i]))
+
+        stdscr.refresh()
+
+        return options
+
+    def get_input(self, stdscr):
+        c = stdscr.getch()
+        options = self.get_options()
+        cont = True
+        if c == ord('q'):
+            cont = False
+        elif c == ord('u'):
+            self.undo()
+        elif c == ord('w'):
+            self.write()
+        elif c in list(map(ord, map(str, map(rowget, options)))):
+            self.choose(numrow.index(int(chr(c))))
+        return cont
+
+    def get_options(self):
+        options = []
+        for i in range(len(self.slists)):
+            if len(self.slists[i]) > 0:
+                options.append(i)
+
+        return options
+
+    def get_status(self):
+        stati = ['-Unsaved changes', '-No changes since save']
+        return stati[len(self.ops) == self.written_ops]
 
     def choose(self, c):
         self.mlist.append(self.slists[c].pop(0))
-        self.ops.append(c)
+
+        def inverse():
+            self.slists[c].insert(0, self.mlist.pop())
+        self.ops.append(inverse)
     
     def undo(self):
         if len(self.ops) > 0:
-            self.slists[self.ops.pop()].insert(0, self.mlist.pop())
+            self.ops.pop()()
+
+    def write(self):
+        self.merge_file.writelines(self.mlist)
+        self.written_ops = len(self.ops)
--- rankm.py
+++ rankm.py
@@ -11,9 +11,6 @@ def cli():
 @click.argument('seperate_files', nargs=-1, type=click.File(mode='r'))
 @click.argument('merge_file', nargs=1, type=click.File(mode='w+'))
 def merge(seperate_files, merge_file):
-    def pfile(f):
-        return f.name
-    click.echo('merge: %s, %s' % (list(map(pfile, seperate_files)), merge_file.name))
     m = Merge(seperate_files, merge_file)
     wrapper(m.main_loop)
 
@@ -23,4 +20,17 @@ def merge(seperate_files, merge_file):
 @click.option('--descending/--ascending', '-d/-a', default=True)
 @click.option('--numbers/--no-numbers', '-n/ ', default=False)
 def display(display_file, descending, numbers):
-    click.echo('display: %s, %s, %s' % (display_file.name, descending, numbers))
+    lines = list(display_file.readlines())
+    r = list(range(1, len(lines) + 1))
+    if descending:
+        r.reverse()
+
+    pre = ''
+    if numbers:
+        pre = '{}. '
+
+    display_list = []
+    for i in r:
+        display_list.append(pre.format(i) + lines[i - 1])
+
+    click.echo(''.join(display_list))
